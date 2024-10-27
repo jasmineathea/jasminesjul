7c2a0bee0d2f8b775bffd44403c43dbec9227dd0
jasmineathea <jna009@uib.no>
2024-10-27 23:34:55 +0100

    Minor updates

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="flex flex-col items-center p-4 bg-sky-200 text-sky-800 relative z-20">
-      <h3 className="text-small font-thin">
+    <footer className="flex flex-col items-center p-4 bg-sky-200 text-slate-400 relative z-20">
+      <h4 className="text-small font-thin">
         kildekoden ligger ute på{" "}
         <Link href="https://github.com/jasmineathea" target="_blank">
-          <span className="text-sky-800 hover:text-sky-400">
+          <span className="text-slate-400 hover:text-slate-500">
             github
           </span>
         </Link>
-        -profilen min! :) </h3>
+        -profilen min! :) </h4>
       <h3 className="text-small font-thin text-slate-500 mt-2">
-        Jasmine A. Næss 🧸 2024 </h3>
+        Jasmine A. Næss 🎅🏽 2024 </h3>
     </footer>
   );
 }
